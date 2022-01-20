1c82d534a417b345c8890bf77d0cdd5fb7813ee9
Leonice <leoniceruvienski@gmail.com>
2022-01-20 19:33:02 -0200

    Aula 02

---
+++ Aula 02 Const e Let/public/bundle.js
@@ -0,0 +1,15 @@
+"use strict";
+
+{
+  var car = 'Palio';
+}
+console.log(car);
+var person = {
+  name: 'Leonice'
+};
+person.name = 'Ronaldo';
+console.log(person);
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
---
+++ Aula 02 Const e Let/src/main.js
@@ -0,0 +1,17 @@
+{
+    var car = 'Palio'
+}
+console.log(car)
+
+
+
+const person = { name: 'Leonice'}
+person.name = 'Ronaldo'
+console.log(person)
+
+
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+
