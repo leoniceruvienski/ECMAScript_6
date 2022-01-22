1b307f3a39607b796b14d5bcdd1ec02ce2172d01
Leonice <leoniceruvienski@gmail.com>
2022-01-22 17:08:43 -0200

    Aula 04

---
+++ Aula 04 Arrow Functions/src/main.js
@@ -0,0 +1,113 @@
+const todos = [
+    {text: 'lavar a louça', done: true},
+    {text: 'apanhar o lixo', done: false},
+    {text: 'dobrar as roupas', done: false},
+]
+
+const doneTodos = todos.filter(function (todo){
+    return todo.done
+})
+console.log(doneTodos)
+
+
+
+const todos = [
+    {text: 'lavar a louça', done: true},
+    {text: 'apanhar o lixo', done: false},
+    {text: 'dobrar as roupas', done: false},
+]
+
+const doneTodos = todos.filter( (todo) => {
+    return todo.done
+})
+console.log(doneTodos)
+
+
+
+
+const todos = [
+    {text: 'lavar a louça', done: true},
+    {text: 'apanhar o lixo', done: false},
+    {text: 'dobrar as roupas', done: false},
+]
+
+const doneTodos = todos.filter(todo => {
+    return todo.done
+})
+console.log(doneTodos)
+
+
+
+const todos = [
+    {text: 'lavar a louça', done: true},
+    {text: 'apanhar o lixo', done: false},
+    {text: 'dobrar as roupas', done: false},
+]
+
+const doneTodos = todos.filter((todo) => todo.done)
+
+console.log(doneTodos)
+
+
+
+function anything (){
+    console.log('Hello Leonice')
+}
+anything()
+
+
+
+const anything = ( ) => console.log('Hello Leonice')
+anything()
+
+
+
+const anything = () => ({name: 'Leonice'})
+console.log(anything())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
