ee11fb744da7860229118f9fb1ffd35983024347
Leonice <leoniceruvienski@gmail.com>
2022-01-29 20:02:20 -0200

    Aula 07

---
+++ Aula 07 Object Short Syntax/src/main.js
@@ -0,0 +1,21 @@
+ const name = 'camisa'
+ const price = 20
+
+ const product = {
+     name: name,
+     price: price,
+     inStock: true
+ }
+ console.log(product)
+
+
+
+ const name = 'camisa'
+ const price = 20
+
+ const product = {
+     name,
+     price,
+     inStock: true
+ }
+ console.log(product)
