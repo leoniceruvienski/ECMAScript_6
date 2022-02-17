1bb0a7b6ec754f100afd58fcceb330b89bbec3a8
Leonice <leoniceruvienski@gmail.com>
2022-02-17 16:02:28 -0200

    aula 16

---
+++ Aula 16 Import - Export/src/calculator.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b
+const subtraction = (a, b) => a - b
+export { sum, subtraction } 
+
+
+
+export const sum = (a, b) => a + b
+export const subtraction = (a, b) => a - b
+
---
+++ Aula 16 Import - Export/src/main.js
@@ -0,0 +1,14 @@
+import {sum, subtraction} from './calculator'
+console.log(sum(5, 5))
+console.log(subtraction(10, 3))
+
+
+
+import * as calculator from './calculator'
+console.log(calculator.sum(5, 5))
+console.log(calculator.subtraction(10, 3))
+
+
+
+import Leonice from './sum'
+console.log(Leonice(5,7))
