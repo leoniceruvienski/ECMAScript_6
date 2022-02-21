ad02ff69765f40d8e8a27df4c0636310eb05a7b2
Leonice <leonice@pop-os.localdomain>
2022-02-21 09:57:30 -0300

    Aula 18

---
+++ Aula 18 Try - Catch/src/main.js
@@ -0,0 +1,36 @@
+const myPromise = () => new Promise((resolve, reject) => {
+    return setTimeout (() => {
+        resolve ('ok')
+    }, 2000)
+})
+
+// myPromise().then(() => {
+//     console.log('primeira vez')
+// }).catch((error) => {
+//     console.warn(error)
+// })
+
+const exec = async () => {
+    try {
+        await myPromise()
+        console.log('primeira vez')
+        await myPromise()
+        console.log('segunda vez')
+    } catch (error) {
+        console.warn(error)
+    } finally {
+        console.log('finalizou')
+    }
+}
+exec()
+ 
+    
+
+
+
+
+
+
+
+
+
