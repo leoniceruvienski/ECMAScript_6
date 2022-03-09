50b054e401f6c63127012650b17ea3da006eba8e
Leonice <leonice@pop-os.localdomain>
2022-03-09 15:21:21 -0300

    Aula 21

---
+++ Aula 22 Introduzindo Vue.js/src/main.js
@@ -0,0 +1,17 @@
+import{ createApp } from 'vue'
+import Todos from './api/todos'
+import './assets/css/main.css'
+
+
+const apiTodos = new Todos()
+const app = createApp({
+    data() {
+        return {
+            todo: '',
+        }
+    },
+    created() {},
+    methods: {},
+})
+
+ app.mount('#app')
---
+++ Aula 22 Introduzindo Vue.js/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const  webpack  = require('webpack')
+
+module.exports = {
+    entry: ['@babel/polyfill', path.resolve(__dirname, './src/main.js')],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js',
+        //sourceMapFilename:'bundle.js.map'   
+    },
+    devServer: {
+        
+        //port: 3000,
+        static: { 
+            directory: path.resolve(__dirname, './public'),            
+        },       
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+        }),
+    ], 
+  
+} 
